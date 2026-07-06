25b8923a3cae27544980dd9c134b157cc269638e
loccidigital <loccidigital@gmail.com>
2026-07-06 15:10:25 +0100

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -120,7 +120,7 @@ export default function About() {
                 Transformando o cenário digital
               </h4>
               <p className="text-gray-500 text-xs sm:text-sm leading-relaxed [text-wrap:pretty]">
-                Atendemos empresas que chegam até nós sem identidade no digital e mostramos o caminho claro para o resultado.
+                Atendemos empresas que chegam até nós sem identidade no digital e mostramos o caminho<br className="sm:hidden" />{" "}claro para o resultado.
               </p>
             </div>
 
